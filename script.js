a1c1c12b168f7c73d2ffd47281a5d323ac6876a6
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-15 17:07:07 +0000

    started work on making calc function
    Needs some more work done to the operation function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const SCREEN = document.querySelector('.screen');
 const KEYS = document.querySelectorAll('.key');
+const OPERATORS = document.querySelectorAll('.operator');
 
 let number1 = '';
 let number2 = '';
 let operator;
+
+let input = '';
+let result;
 
 function add(a, b) { 
     return a + b;
@@ -21,26 +25,61 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, number1, number2) {
+function operate() {
     if (operator === '+') {
-        add(operator, number1, number2);
+        return add(Number(number1), Number(number2));
     } else if (operator === '-') {
-        subtract(operator, number1, number2);
+        return subtract(Number(number1), Number(number2));
     } else if (operator === 'x') {
-        subtract(operator, number1, number2);
-    } else if (operator === '%') {
-        subtract(operator, number1, number2);
+        return multiply(Number(number1), Number(number2));
+    } else if (operator === '/') {
+        return divide(Number(number1), Number(number2));
     }
+}
+
+function evaluate() {
+    // set result
+    result = operate();
+    // update screen
+    SCREEN.textContent = result;
 }
 
 KEYS.forEach(key => {
     key.addEventListener('click', (e) => {
         
-        // Update number variable
-        number1 += key.textContent;
+        // update input
+        input += key.textContent;
 
-        // Update screen
-        SCREEN.textContent = number1
+        // update screen
+        SCREEN.textContent = input;
 
+    })
+})
+
+OPERATORS.forEach(op => {
+    op.addEventListener('click', (e) => {
+        if (!op.textContent === '=') {
+            if (number1 !== '' && number2 !== '') {
+                // calculate previous numbers first
+                evaluate();
+                // set new operator
+                operator = op.textContent;
+            }
+            if (number1 !== '' && number2 === '') {
+                // store input
+                number1 = input;
+                // set new operator
+                operator = op.textContent;
+                // reset input
+                input = '';
+            } else if (number2 !== '' && number1 === '') {
+                // store input
+                number2 = input;
+                // set new operator
+                operator = op.textContent;
+                // reset input
+                input = '';
+            }
+        }      
     })
 })
